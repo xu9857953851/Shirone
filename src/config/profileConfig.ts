4ef66e42c477ae00c7e18c621c4d13432866a859
xu9857953851 <95112711+xu9857953851@users.noreply.github.com>
2026-09-09 02:26:18 +0800

    chore: 修改博主的用户名为 无敌冰

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -7,7 +7,7 @@ import { withUserConfig } from "../utils/config-overlay.ts";
  */
 export const profileConfig: ProfileConfig = withUserConfig("profile", {
 	avatar: "assets/images/demo-avatar.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Shirone",
+	name: "无敌冰",
 	bio: "The rain remembers what the sky forgot to say.",
 	links: [
 		{
